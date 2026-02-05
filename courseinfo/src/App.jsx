302ffc1e0bf86836102c6595b7bcc2bac374e999
skarang1 <skarang1@asu.edu>
2026-02-05 13:35:49 -0700

    maded changes so the componets will render without errors.

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,25 +1,19 @@
 const Header = (props) =>{
   return(
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   )
 }
 
-const Part=(props)=>{
-  return(
-  <>
-    <p>{props.name} {props.exercises}</p>
-  </>
-  )
-}
 
 const Content = (props) =>{
+  const parts = props.course.parts
   return(
     <>
-      <Part name={props.part1} exercises={props.exercises1}/>
-      <Part name={props.part2} exercises={props.exercises2}/>
-      <Part name={props.part3} exercises={props.exercises3}/>
+      <p>{parts[0].name} {parts[0].exercises}</p>
+      <p>{parts[1].name} {parts[1].exercises}</p>
+      <p>{parts[2].name} {parts[2].exercises}</p>
     </>
   )
 }
@@ -35,28 +29,30 @@ const Total = (props) =>{
 
 const App = () => {
 
-  const course = 'Half Stack application development'
-  const parts =[
-  {
-    name:'Fundamentals of React',
-    exercises: 10
+  const course = {
+  name: 'Half Stack application development',
+  parts: [
+      {
+        name:'Fundamentals of React',
+        exercises: 10
+      }
+      ,{
+        name:'Using props to pass data',
+        exercises: 7
+      }
+      ,{
+        name:'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  ,{
-    name:'Using props to pass data',
-    exercises: 7
-  }
-  ,{
-    name:'State of a component',
-    exercises: 14
-  }
-]
   
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Content course={course}/>
+      <Total total = {course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises}/>
     </div>
   )
 }
